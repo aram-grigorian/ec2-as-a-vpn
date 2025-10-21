6802a02feac7ecb61145cb196cfa8207b01ab371
ramgrigoryan@protonmail.com <ramgrigoryan@protonmail.com>
2025-10-21 14:24:43 +0400

    Added VPC TF Files

---
+++ vpc/main.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpn_vpc
+  cidr = var.main_cidr
+
+  azs             = var.azs_list
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+
+  #   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  #   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  #   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
